8cff9c00329dbfb10d0ccdbfcdc860d92372e503
Simon Hulse <simonhulse@protonmail.com>
2024-12-09 17:23:25 -0500

    AoC: setup for Y 2024 D 02 in java

---
+++ advent-of-code/java/src/main/java/org/simonhulse/aoc/Y2024D02Solution.java
@@ -0,0 +1,18 @@
+//src/main/java/org/simonhulse/aoc/Y2024D02Solution.java
+//Simon Hulse
+//simonhulse@protonmail.com
+//Last Edited: Mon 09 Dec 2024 05:21:53 PM EST
+
+package org.simonhulse.aoc;
+
+public class Y2024D02Solution extends AOCSolution {
+
+    public int partOne(String input) {
+        return 0;
+    };
+
+    public int partTwo(String input) {
+        return 0;
+    };
+
+}
--- advent-of-code/java/src/test/java/org/simonhulse/aoc/Y2024D02Test.java
+++ advent-of-code/java/src/test/java/org/simonhulse/aoc/Y2024D02Test.java
@@ -1,7 +1,7 @@
-//src/test/java/org/simonhulse/aoc/Y2024D01Test.java
+//src/test/java/org/simonhulse/aoc/Y2024D02Test.java
 //Simon Hulse
 //simonhulse@protonmail.com
-//Last Edited: Mon 09 Dec 2024 05:22:45 PM EST
+//Last Edited: Mon 09 Dec 2024 05:22:28 PM EST
 
 package org.simonhulse.aoc;
 
@@ -9,15 +9,15 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
-public class Y2024D01Test extends AOCTest {
+public class Y2024D02Test extends AOCTest {
 
-    public Y2024D01Test() throws IOException {
+    public Y2024D02Test() throws IOException {
         super();
     }
 
     @Override
     public int getDay() {
-        return 1;
+        return 2;
     }
 
     @Override
